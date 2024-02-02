81d5ce1a4a39398a0756b595dc2caab887b0ba88
Olabode Balinga <98917947+Rezix15@users.noreply.github.com>
2024-02-02 22:53:28 +0000

    Attemption the addition of skill upgrades.

---
+++ Starlight Racers/Assets/Scripts/BoosterSkillObj.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Booster Skill", menuName = "BoostSkill")]
+public class BoosterSkillObj : ScriptableObject
+{
+    public new string name;
+}
